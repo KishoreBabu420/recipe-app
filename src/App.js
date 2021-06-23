49cff6d994529d0a590aeeeb96bd730a412180ee
Kishore <kishore.sai37@gmail.com>
2021-06-23 19:26:03 +0530

    recipie app completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+import RecipeWindow from './components/RecipeWindow';
 
 function App() {
+  const YOUR_APP_ID = '55bdb0d0';
+  const YOUR_APP_KEY = 'cdff2bac3a8f6f5271f3308ca11e0b10';
+
+  const [query, setQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipes = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const submitHandler = function (e) {
+    e.preventDefault();
+    getRecipes();
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Search Your Recipe</h1>
+      {/* Search Form */}
+      <form className="form-control" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Search your Main Ingredient"
+          className="input-search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input type="submit" value="Submit" className="btn-submit" />
+      </form>
+
+      <RecipeWindow recipes={recipes} />
     </div>
   );
 }
